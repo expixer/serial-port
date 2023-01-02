20150c3049c1900507f97c902bde8be1b461aa06
Sezgin Sevinc <sezginex@gmail.com>
2023-01-02 18:00:52 +0300

    Proje dosyası ekle.

---
+++ SeriPortProgrami/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace SeriPortProgrami
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBoxSeriPortlar = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.textBoxMessages = new System.Windows.Forms.TextBox();
+            this.textBoxMyMessage = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxSeriPortlar
+            // 
+            this.comboBoxSeriPortlar.FormattingEnabled = true;
+            resources.ApplyResources(this.comboBoxSeriPortlar, "comboBoxSeriPortlar");
+            this.comboBoxSeriPortlar.Name = "comboBoxSeriPortlar";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // buttonConnect
+            // 
+            resources.ApplyResources(this.buttonConnect, "buttonConnect");
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonDisconnect
+            // 
+            resources.ApplyResources(this.buttonDisconnect, "buttonDisconnect");
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // textBoxMessages
+            // 
+            resources.ApplyResources(this.textBoxMessages, "textBoxMessages");
+            this.textBoxMessages.Name = "textBoxMessages";
+            // 
+            // textBoxMyMessage
+            // 
+            resources.ApplyResources(this.textBoxMyMessage, "textBoxMyMessage");
+            this.textBoxMyMessage.Name = "textBoxMyMessage";
+            // 
+            // buttonSend
+            // 
+            resources.ApplyResources(this.buttonSend, "buttonSend");
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxMessages);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxMyMessage);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.comboBoxSeriPortlar);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxSeriPortlar;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.TextBox textBoxMessages;
+        private System.Windows.Forms.TextBox textBoxMyMessage;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SeriPortProgrami/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace SeriPortProgrami
+{
+    public partial class Form1 : Form
+    {
+        string fileName = DateTime.Now.ToString("yyyy_MM_dd HH_mm_ss");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonDisconnect.Enabled = false;
+            buttonSend.Enabled = false;
+            foreach (var serialPort in SerialPort.GetPortNames()){
+                comboBoxSeriPortlar.Items.Add(serialPort);
+            }
+            if (comboBoxSeriPortlar.Items.Count > 0){
+                comboBoxSeriPortlar.SelectedIndex = 0;
+            }
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+            string dosya = path + "\\" + fileName + ".txt";
+            File.AppendAllText(dosya, "tarih,sicaklik1,sicaklik2,sicaklik3,sicaklik4,sicaklik5 \n");
+
+
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = comboBoxSeriPortlar.Text;
+            serialPort1.BaudRate = 57600;
+            serialPort1.Parity = Parity.None;
+            serialPort1.StopBits = StopBits.One;
+            serialPort1.DataBits = 8;
+
+
+            try
+            {
+                serialPort1.Open();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show($"Seri port bağlantısı yapılamadı\n Hata : {ex.Message}","Problem",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            
+            if (serialPort1.IsOpen)
+            {
+                buttonConnect.Enabled = false;
+                buttonDisconnect.Enabled = true;
+                buttonSend.Enabled = true;
+            }
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                buttonConnect.Enabled = true;
+                buttonDisconnect.Enabled = false;
+                buttonSend.Enabled = false;
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Write(textBoxMyMessage.Text);
+                textBoxMyMessage.Clear();
+            }
+        }
+        public delegate void veriGoster(String s);
+
+        public void texteYaz(String s)
+        {
+            textBoxMessages.Text += s;
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            String gelenVeri = DateTime.Now.ToString("yy-MM-dd HH:mm:ss,") + serialPort1.ReadExisting();
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string dosya = path + "\\"+ fileName + ".txt";
+            string kayit = gelenVeri ;
+
+                string[] bolunenKayit = kayit.Split(',');
+                
+                //label1.Text = kayit.Split(',')[0];
+            File.AppendAllText(dosya, bolunenKayit.Length.ToString());
+            //textBoxMessages.Text += gelenVeri;
+            textBoxMessages.Invoke(new veriGoster(texteYaz), bolunenKayit.Length.ToString());
+        }
+    }
+}
